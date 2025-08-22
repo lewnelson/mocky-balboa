a3076d9f3236b81f7c426accf6d8b66084ae1486
Lewis Nelson <lewis@lewnelson.com>
2025-08-22 17:29:07 +0100

    fix: gh action

--- examples/next-js-14/src/app/page.tsx
+++ examples/next-js-14/src/app/page.tsx
@@ -13,6 +13,8 @@ import {
   TrainingRegime,
 } from "@/lib/data";
 
+export const dynamic = "force-dynamic";
+
 export default async function Home() {
   const [[nextFight, nextFightError], [trainingRegime, trainingRegimeError]] =
     await Promise.all([getNextFight(), getTrainingRegime()]);
